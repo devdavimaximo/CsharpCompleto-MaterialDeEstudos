1b274405f062b4b0b1544d424da74aff60f0f13d
Davi Maximo Quoos <davimaximoquooss@gmail.com>
2026-02-19 22:38:38 -0300

    docs: adiciona conteudo de modulo 4

---
+++ 04 - ComportamentoDeMemória Arrays, Lista/08 - Foreach/foreach/foreach/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Studyforeach;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string[] vect = new string[] { "Nayara", "Davi", "Jesus" };
+        foreach (string name in vect)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
