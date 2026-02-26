bcc12e61e9a8e6d7d6f88b0f67d6d51279f4b15d
Davi Maximo Quoos <davimaximoquooss@gmail.com>
2026-02-25 22:46:20 -0300

    docs: adiciona novos materiais de estudos modulo 5

---
+++ 05 - Tópicos Especiais em C#/05 - DateTime/dateTime/dateTime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace dateTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Now;
+            Console.WriteLine(d1);
+            Console.WriteLine(d1.Ticks);
+
+            DateTime d2 = new DateTime(2022, 10, 29);
+            DateTime d3 = new DateTime(2022, 10, 29, 11, 30, 00);
+            DateTime d4 = DateTime.Today;
+            DateTime d5 = DateTime.UtcNow;
+
+            Console.WriteLine(d2);
+            Console.WriteLine(d3);
+            Console.WriteLine(d4);
+            Console.WriteLine(d5);
+
+            //
+
+            DateTime n1 = DateTime.Parse("2022-10-29");
+            Console.WriteLine(n1);
+
+            DateTime n2 = DateTime.ParseExact("2027-10-29", "yyyy-MM-dd", CultureInfo.InvariantCulture);
+            Console.WriteLine(n2);
+        }
+    }
+}
---
+++ 05 - Tópicos Especiais em C#/06 - TimeSpan/TimeSpan/TimeSpan/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace timespan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan t1 = new TimeSpan(1, 2, 3); // 1 hour, 2 minutes, 3 seconds
+            Console.WriteLine(t1);
+
+            TimeSpan t2 = TimeSpan.FromMinutes(90); // 1 hour, 30 minutes
+            Console.WriteLine(t2);
+        }
+    }
+}
